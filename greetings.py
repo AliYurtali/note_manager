f740d3c9c1a350cecb9349292f7ce644f33eb358
AliYurtali <alya.yurtaeva.98@gmail.com>
2024-11-30 11:02:00 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+#created_date - "день-месяц-год", например "10-11-2024"
+#issue_date - ", например "10-12-2024"
+username = 'Alina'
+title = 'Лекция 1'
+content = 'Посмотреть лекцию 1'
+status = 'Просмотрена лекция 1'
+created_date = '29-11-2024'
+issue_date = '10-12-2024'
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дедлайн:", issue_date)
