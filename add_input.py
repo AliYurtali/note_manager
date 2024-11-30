be5ef5a4fc21cce2a898e6b012cfc48d91879bfb
AliYurtali <alya.yurtaeva.98@gmail.com>
2024-11-30 16:29:33 +0500

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,15 @@
+username = input("Введите имя: ")
+title = input("Введите заголовок заметки: ")
+content = input("Введите описание заметки: ")
+status = input("Введите статус заметки: ")
+created_date = input("Введите дату создания заметки (ДД.ММ.ГГГГ): ")
+issue_date = input("Введите дату истечения заметки (ДД.ММ.ГГГГ): ")
+temp_created_date = '29.11'
+temp_issue_date = '10.12'
+fimal = { (username),
+        (title),
+        (content),
+        (status),
+        (created_date),
+        (issue_date) }
+print(fimal)
