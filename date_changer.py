43a5472bb4d7fbb2adb589ea92f699e103a5af89
Alina <alya.yurtaeva.98@gmail.com>
2024-12-08 18:29:35 +0500

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -4,11 +4,11 @@ content = 'Посмотреть лекцию 1'
 status = 'Просмотрена лекция 1'
 created_date = '29.11.2024'
 issue_date = '10.12.2024'
-temp_created_date = '29.11'
-temp_issue_date = '10.12'
+#temp_created_date = []
+#temp_issue_date = '10.12'
 print("Имя пользователя:", username)
 print("Заголовок заметки:", title)
 print("Описание заметки:", content)
 print("Статус заметки:", status)
-print("Дата создания заметки:", temp_created_date)
-print("Дедлайн:", temp_issue_date)
+print("Дата создания заметки:", (created_date)[0:5])
+print("Дедлайн:", (issue_date)[0:5])
